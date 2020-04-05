5987c324785b85f4283cdc978208b5b15c460e97
Sniedze Spele <spele.sniedze@gmail.com>
2020-04-05 23:37:40 +0300

    formating

--- home.php
+++ home.php
@@ -22,80 +22,82 @@ if(!isset($_SESSION['username'])){
     header('index.php');
 } 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magebit</title>
-    <script src="styles/script.js" defer></script>
-    <link href="styles/style.css?version=51" rel="stylesheet" type="text/css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Magebit</title>
+        <script src="styles/script.js" defer></script>
+        <link href="styles/style.css?version=51" rel="stylesheet" type="text/css">
 
-</head>
+    </head>
 
-<body>
-    
+    <body>
+
         <header></header>
         <div class=content2>
-            <div class= "whitebg">
-<div class="heading">
-		<h2>Here you can add information about yourself</h2>
-	</div>
-	<form id="add" method="post" action="home.php" class="input_form">
-    <?php if (isset($errors)) { ?>
-	<p><?php echo $errors; ?></p>
-<?php } ?>
-    <label for="email">Statement<span style="color:red">*</span></label>
-    <br>
-        <input type="text" name="task" class="task_input">
-        <br>
-        <br>
-        <label for="email">Answer<span style="color:red" class=ls>*</span></label>
-        <br>
-        <input type="text" name="task" class="task_input">
-        <br>
-		<button type="submit" name="submit" id="add_btn" class="addbtn">Add it</button>
-    </form>
-    <br>
-    <br>
-    <br>
-    <div class=heading>
-    <h2>Personal information</h2>
-</div>
+            <div class="whitebg">
+                <div class="heading">
+                    <h2>Here you can add information about yourself</h2>
+                </div>
+                <form id="add" method="post" action="home.php" class="input_form">
+                    <?php if (isset($errors)) { ?>
+                        <p>
+                            <?php echo $errors; ?>
+                        </p>
+                        <?php } ?>
+                            <label for="email">Statement<span style="color:red">*</span></label>
+                            <br>
+                            <input type="text" name="task" class="task_input">
+                            <br>
+                            <br>
+                            <label for="email">Answer<span style="color:red" class=ls>*</span></label>
+                            <br>
+                            <input type="text" name="task" class="task_input">
+                            <br>
+                            <button type="submit" name="submit" id="add_btn" class="addbtn">Add it</button>
+                </form>
+                <br>
+                <br>
+                <br>
+                <div class=heading>
+                    <h2>Personal information</h2>
+                </div>
 
-<table style="width:90%">
-  <tr>
-    <th>Statement</th>
-    <th>Answer</th> 
-  </tr>
-  <br>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-</table>
-    </div>
-</div>
-</div>
-    <footer>
+                <table style="width:90%">
+                    <tr>
+                        <th>Statement</th>
+                        <th>Answer</th>
+                    </tr>
+                    <br>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        </div>
+        <footer>
             <div class="footerdiv">
                 <p>All Rights Reserved “Magebit” 2016</p>
             </div>
         </footer>
-    </div>
-    </div>
-</body>
+        </div>
+        </div>
+    </body>
 
-</html>
+    </html>
--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <?php
 require_once 'head.php';
 ?>
-        <div class="container">
-            <div class="content">
-                <div class="registrate">
-                    <h3>Don’t have an account?</h3>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit,
-                        sed do<br> eiusmod tempor incididunt ut labore et<br> dolore magna aliqua.</p>
-                    <button type="button" onclick="todo()" class="signupbtn1">Sign Up</button>
-                </div>
-                <div class="for-login">
-                    <h3>Have an account?</h3>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit.</p>
-                    <button class="loginbtn1" type="button" onclick="todo2()">Log in</button>
-                </div>
-                <div id="slider">
-                    <div class="sign-up" id="foo2">
+    <div class="container">
+        <div class="content">
+            <div class="registrate">
+                <h3>Don’t have an account?</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet,
+                    <br> consectetur adipisicing elit, sed do
+                    <br> eiusmod tempor incididunt ut labore et
+                    <br> dolore magna aliqua.</p>
+                <button type="button" onclick="todo()" class="signupbtn1">Sign Up</button>
+            </div>
+            <div class="for-login">
+                <h3>Have an account?</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet consectetur
+                    <br> adipisicing elit.</p>
+                <button class="loginbtn1" type="button" onclick="todo2()">Log in</button>
+            </div>
+            <div id="slider">
+                <div class="sign-up" id="foo2">
                     <div id="triangle-bottomright"></div>
-                        <div id="square"></div>
-                        <h3 id="title">Sign Up<img src="styles/img/logo.jpg" class="logo" alt="logo">
+                    <div id="square"></div>
+                    <h3 id="title">Sign Up<img src="styles/img/logo.jpg" class="logo" alt="logo">
                         </h3>
-                        <hr>
-                        <br>
-                        <form action="registration.php" method="post">
-<label for="name">Name<span style="color:red">*</span></label>
+                    <hr>
+                    <br>
+                    <form action="registration.php" method="post">
+                        <label for="name">Name<span style="color:red">*</span></label>
                         <br>
                         <input class="test-input" type="text" name="user" required />
                         <div class="input-icon"><img src="styles/img/profil.jpg" class="profil"></div>
@@ -41,33 +44,33 @@ require_once 'head.php';
                         <div class="input-icon"><img src="styles/img/lock.jpg" class="profil"></div>
                         <br>
                         <button type="submit" class="signupbtn">Sign Up</button>
-</form>
-                        <div id="triangle-topright2"></div>
-                    </div>
-                    <div class="login" id="foo">
-                        <div id="triangle-bottomright"></div>
-                        <div id="square"></div>
-                        <h3 id="title">Login<img src="styles/img/logo.jpg" class="logo" alt="logo"></h3>
-                        <hr>
+                    </form>
+                    <div id="triangle-topright2"></div>
+                </div>
+                <div class="login" id="foo">
+                    <div id="triangle-bottomright"></div>
+                    <div id="square"></div>
+                    <h3 id="title">Login<img src="styles/img/logo.jpg" class="logo" alt="logo"></h3>
+                    <hr>
+                    <br>
+                    <form action='validation.php' method='post'>
+                        <label for='email'>Email<span style='color:red'>*</span></label>
                         <br>
-                        <form action='validation.php' method='post'>
-<label for='email'>Email<span style='color:red'>*</span></label>
-                       <br>
- <input class='test-input' type='text' name='email' required />
- <div class='input-icon'><img src='styles/img/email.jpg' class='profil'></div>
+                        <input class='test-input' type='text' name='email' required />
+                        <div class='input-icon'><img src='styles/img/email.jpg' class='profil'></div>
                         <br>
-                        <label for= 'psw'>Password<span style='color:red'>*</span></label>
+                        <label for='psw'>Password<span style='color:red'>*</span></label>
                         <br>
                         <input class='test-input' type="password" name="password" required />
                         <div class='input-icon'><img src='styles/img/lock.jpg' class='profil'></div>
                         <br>
                         <button type='submit' class='loginbtn'>Log in</button>
-</form>
-                        <div id="triangle-topright"></div>
-                    </div>
-
+                    </form>
+                    <div id="triangle-topright"></div>
                 </div>
+
             </div>
-            </div>
-        <?php
+        </div>
+    </div>
+    <?php
         require_once 'footer.php';
