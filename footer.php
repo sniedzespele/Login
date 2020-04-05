c5d82bc719ed15dc40a38eecd0e0b47f688c3183
Sniedze Spele <spele.sniedze@gmail.com>
2020-04-06 00:16:35 +0300

    format

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-
 <footer>
-            <div class="reserved">
-                <p>All Rights Reserved “Magebit” 2016</p>
-            </div>
-        </footer>
+    <div class="reserved">
+        <p>All Rights Reserved “Magebit” 2016</p>
     </div>
+</footer>
+</div>
 </body>
 
 </html>
--- styles/script.js
+++ styles/script.js
@@ -1,14 +1,18 @@
 function todo() {
-    myMove(); myFunction();
+    myMove();
+    myFunction();
 }
+
 function todo2() {
-    myMove2(); myFunction2();
+    myMove2();
+    myFunction2();
 }
 
 function myMove() {
     var elem = document.getElementById("slider");
     var pos = 10;
     var id = setInterval(frame, 10);
+
     function frame() {
         if (pos >= 445) {
             pos = 445
@@ -20,10 +24,12 @@ function myMove() {
         }
     }
 }
+
 function myMove2() {
     var elem = document.getElementById("slider");
     var pos = 0;
     var id = setInterval(frame, 10);
+
     function frame() {
         if (pos >= 445) {
             pos = 445
@@ -35,12 +41,13 @@ function myMove2() {
         }
     }
 }
+
 function myFunction() {
     document.getElementById("foo").style.visibility = "hidden";
     document.getElementById("foo2").style.visibility = "visible";
 }
+
 function myFunction2() {
     document.getElementById("foo2").style.visibility = "hidden";
     document.getElementById("foo").style.visibility = "visible";
 }
-
